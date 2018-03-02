8c7134d169a840fdf025726fad89eb721d8fd1a4
Kingsley Owusu-Sekyere <kingsley.owusu-sekyere@maximago.de>
2018-03-02 15:13:56 +0100

    new cli component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 import { PlayerInfoComponent } from './player-info/player-info.component';
 import { PlayerService } from './shared/services/player.service';
+import { PlayerEditComponent } from './player-edit/player-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerListComponent,
-    PlayerInfoComponent
+    PlayerInfoComponent,
+    PlayerEditComponent
   ],
   imports: [
       BrowserModule,
