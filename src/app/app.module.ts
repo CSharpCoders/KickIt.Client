beb64b9ba7242e003060eb95733d2229fa145a99
Kingsley Owusu-Sekyere <kingsley.owusu-sekyere@maximago.de>
2018-03-02 14:42:41 +0100

    add http, implement getPlayers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 
@@ -15,7 +16,8 @@ import { PlayerService } from './shared/services/player.service';
     PlayerInfoComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpClientModule
   ],
     providers: [
         PlayerService
--- src/app/shared/services/player.service.ts
+++ src/app/shared/services/player.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
+import { catchError } from 'rxjs/operators';
+
+import { Player } from './../models/player.model';
 
 @Injectable()
 export class PlayerService {
+    private url: string;
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
+    public getPlayers(): Observable<Player[]> {
+        return this.http.get<Player[]>(this.url)
+            .pipe(
+                catchError(this.handleError('getPlayers', []))
+            );
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+
+            // TODO: send the error to remote logging infrastructure
+            console.error('operation' + error); // log to console instead
+
+            // TODO: better job of transforming error for user consumption
+            console.log(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
 
 }
